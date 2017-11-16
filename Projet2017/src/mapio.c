13e3d6955d44f83e3114ab9642c6577a82ef04b5
Heritiana Mahazoasy <hmahazoasy@emi.u-bordeaux1.fr>
2017-11-16 17:22:59 +0100

    fin partie 1 projet

--- Projet2017/src/mapio.c
+++ Projet2017/src/mapio.c
@@ -11,7 +11,7 @@
 
 void map_new (unsigned width, unsigned height)
 {
-    map_allocate (width, height);
+  map_allocate (width, height);
 
   for (int x = 0; x < width; x++)
     map_set (x, height - 1, 0); // Ground
@@ -36,188 +36,144 @@ void map_new (unsigned width, unsigned height)
   // Petit plancher flottant
   map_object_add ("images/floor.png", 1, MAP_OBJECT_SEMI_SOLID);
 
-  // Ajout des fleurs
-  map_object_add("images/flower.png",1,MAP_OBJECT_AIR);
-  // Ajout  des pièces
-  map_object_add("images/coin.png",20,MAP_OBJECT_AIR || MAP_OBJECT_COLLECTIBLE);
+  // Fleur pour égayer le paysage
+  map_object_add("images/flower.png", 1, MAP_OBJECT_AIR);
+  //Pièces à collecter
+  map_object_add("images/coin.png",20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);
 
   map_object_end ();
 }
 
 void map_save (char *filename)
 {
-   // fprintf (stderr, "Sorry: Map save is not yet implemented\n");
+  int file = open(filename,O_CREAT | O_WRONLY|O_TRUNC , 0666);
 
-  // TODO
-  if(filename == NULL){
-    fprintf (stderr, "Sorry: Map save is not yet implemented\n");
+
+  //On recupere la largeur de la carte puis la hauteur
+  int width = map_width();
+  int height = map_height();
+  int nbObjet = map_objects(); // on recupere le nombre d'objets differents
+  //On écrit dans le fichier la largeur, la hauteur et le nombre d'objets differents
+  write(file,&width,sizeof(int));
+  write(file,&height,sizeof(int));
+  write(file,&nbObjet,sizeof(int));
+
+  int frame, solidity, isDestructible, isGenerator, isCollectible;
+  char* fichierPNG;
+  int taille;
+
+ //on envoie tous les objets différents de la map
+  for (int i = 0; i < nbObjet; i++) {
+    fichierPNG = map_get_name(i);
+    taille = strlen(fichierPNG);
+    //on envoie dans le fichier la longueur du nom de l'objet
+    write(file,&taille,sizeof(int));
+    //tant qu'on arrive pas à la fin du nom de l'objet, on envoie caractère par caractère dans le file
+    for(int indice=0;indice < taille;indice++) {
+      write(file,&fichierPNG[indice], sizeof(char));
+    }
+    /* On ecrit dans le fichier le nom,le nombre de sprites du fichier ainsi que les
+      les propriétés qu'il peut avoir
+    */
+    frame = map_get_frames(i);
+    write(file,&frame,sizeof(int));
+
+
+    solidity = map_get_solidity(i);
+    write(file,&solidity,sizeof(int));
+
+
+    isDestructible = map_is_destructible(i);
+    write(file,&isDestructible,sizeof(int));
+
+
+    isCollectible = map_is_collectible(i);
+    write(file,&isCollectible,sizeof(int));
+
+
+    isGenerator = map_is_generator(i);
+    write(file,&isGenerator,sizeof(int));
 
   }
-//  filename = "sauvegarde.txt";
-  int fd = open(filename,O_WRONLY | O_CREAT | O_TRUNC,0666);
-  int width =  map_width();
-  int height = map_height();
-  int nbObjet = map_objects();
-
-  char buffer[10];
-
-
-  sprintf(buffer,"%d\n",width);
-  write(fd,&buffer,sizeof(buffer));
-  write(fd,"!",sizeof(char));
-  sprintf(buffer,"%d\n",height);
-  write(fd,&buffer,sizeof(buffer));
-  write(fd,"!",sizeof(char));
-
-  sprintf(buffer,"%d\n",nbObjet);
-  write(fd,&buffer,sizeof(buffer));
-  write(fd,"!",sizeof(char));
 
   int contenuCase;
   for (int i = 0; i < width; i++) {
     for (int j = 0; j < height; j++) {
-      contenuCase = map_get(i,j);
-
-      if(contenuCase != MAP_OBJECT_NONE){
-        char *fichierPNG = map_get_name(contenuCase);
-        int frames = map_get_frames(contenuCase);
-        int solidity = map_get_solidity(contenuCase);
-
-        int isDestructible = map_is_destructible(contenuCase);
-        int isCollectible = map_is_collectible(contenuCase);
-        int isGenerator = map_is_generator(contenuCase);
-        write(fd,"!",sizeof(char));//début élement
-        write(fd,&fichierPNG,sizeof(strlen(fichierPNG)));
-        write(fd,"!",sizeof(char));//fin du nom de l'élement
-
-        sprintf(buffer,"%d",frames);
-        write(fd,&buffer,sizeof(buffer));
-        write(fd,"!",sizeof(char));//fin de la taille du frame
-
-        sprintf(buffer,"%d",solidity);
-        write(fd,&buffer,sizeof(buffer));
-
-        sprintf(buffer,"%d",isDestructible);
-        write(fd,&buffer,sizeof(buffer));
-
-        sprintf(buffer,"%d",isCollectible);
-        write(fd,&buffer,sizeof(buffer));
-
-        sprintf(buffer,"%d",isGenerator);
-        write(fd,&buffer,sizeof(buffer));
-
-      }
-      else
-        write(fd,"0",sizeof(char));
+      contenuCase = map_get(i,j); //numéro de l'objet dans la liste des objets de la map
+      write(file,&contenuCase,sizeof(int));
     }
   }
-  close(fd);
-  printf("save done i guess");
 
+  close(file);
+  fprintf (stderr, "Done: Map save is implemented\n");
 }
 
 void map_load (char *filename)
 {
-   // TODO
+  int file = open(filename,O_RDONLY);
+  int height, width, nbObject;
 
-   int width,height,nbObject,frames,solidity,isDestructible,isCollectible,isGenerator;
-   char *fichierPNG;
-   int file=open(filename,O_RDONLY,0666);
-   char *tmp;
-   char *word;
+  //Lecture de la hauteur et de la largeur
+  read(file,&width,sizeof(int));
+  read(file,&height,sizeof(int));
 
-   int pos_x,pos_y;
+  map_allocate(width,height);
 
+  read(file,&nbObject,sizeof(int));
+  map_object_begin(nbObject); //début initialisation de la liste des objets de la map
 
+  char* fichierPNG=NULL;
+  int taille=-1 , nbSprite, properties;
+  int buffer; //va contenir toutes les informations récupérés lors du read.
+              //Il représentera les propriétés des objets de la map
+              //mais aussi le contenu des éléments de la matrice qui permettra d'écrire
+              //sur la map via map_set(x,y,buffer)
 
+  for (int i = 0; i < nbObject; i++) {
+    read(file,&buffer,sizeof(int)); //compte le nombre de caractère dans le nom du des objets
+    if (buffer> taille) {
+      fichierPNG = realloc(fichierPNG, buffer*sizeof(char));
+      taille = buffer;
+    }
+    for (int j = 0; j < buffer; j++) {
+      read(file,&fichierPNG[j], sizeof(char));
+    }
 
-   //sequence à répéter pour chaque données à taille variable du fichier de sauvegarde, peut être simplifier par une fonction int f(int fd) qui lit jusqu'au prochain '!'
-   //width
-   read(file,tmp,sizeof(char));
-   while(strcmp(tmp,"!")){
-      strcat(word,tmp);
-//      str_append(word,tmp);
-      read(file,tmp,sizeof(char));
-   }
-   width=atoi(word);
-   word="";
-   //height
-   read(file,tmp,sizeof(char));
-   while(strcmp(tmp,"!")){
-      strcat(word,tmp);
-      read(file,tmp,sizeof(char));
-   }
-   height=atoi(word);
-   word="";
-   //nbObject
-    read(file,tmp,sizeof(char));
-    while(strcmp(tmp,"!")){
-      strcat(word,tmp);
-      read(file,tmp,sizeof(char));
-   }
-   nbObject=atoi(word);
-   word="";
+    fichierPNG[buffer] = '\0';//fin du nom du fichierPNG
+    read(file,&nbSprite,sizeof(int));
+    read(file,&properties,sizeof(int));
+    read(file,&buffer,sizeof(int));
 
-//Création de la map, ajout des élements
-   map_allocate(width,height);
-   char *tabObject[nbObject]; //permettra d'étudier si un object a déjà été add dans la map
-   int nbElmnt=0;
-   map_object_begin(nbObject);
+    //cas sur les proprietes de l'objet : MAP_OBJECT_COLLECTIBLE, MAP_OBJECT_COLLECTIBLE, MAP_OBJECT_GENERATOR
+    if (buffer) {
+      properties =  properties | MAP_OBJECT_DESTRUCTIBLE;
+    }
+    read(file,&buffer,sizeof(int));
+    if (buffer) {
+      properties =  properties | MAP_OBJECT_COLLECTIBLE;
+    }
+    read(file,&buffer,sizeof(int));
 
-   //début lecture matrice terrain
-   for(int i = 0;i <width; i++){
-      for(int j = 0; j < height ; j++){
-         if(strcmp(tmp,"!")){
-            //début read des éléments
-            //je le ferai plus tard wallah
-            while(strcmp(tmp,"!")){
-               strcat(word,tmp);
-               read(file,tmp,sizeof(char));
-            }
-            fichierPNG=word;
-            word="";
-            while(strcmp(tmp,"!")){
-               strcat(word,tmp);
-               read(file,tmp,sizeof(char));
-             }
-            frames=atoi(word);
-            word="";
-            read(file,word,sizeof(char));
-            solidity=atoi(word);
-            read(file,word,sizeof(char));
-            isDestructible=atoi(word);
-            read(file,word,sizeof(char));
-            isCollectible=atoi(word);
-            read(file,word,sizeof(char));
-            isGenerator=atoi(word);
-            //fin lecture des informations de l'élément
-            read(file,tmp,(sizeof(char)));//passage au prochain caractère
-            pos_x=i;
-            pos_y=j;
-            int k=0;
-            while(k<nbObject && (strcmp(fichierPNG,tabObject[k]))!=0){ //on vérifie que l'élement n'a pas déjà été ajouté
-               k++;
-            }
-            if(k==nbObject){ //s'il n'a jamais été ajouté on fait map_object _add et on l'ajoute sur la map
-               map_object_add(fichierPNG,frames,solidity);//doit mettre en avant isDestructible etc....
-               tabObject[nbElmnt]=fichierPNG;
-               nbElmnt++;
-               map_set(pos_x,pos_y,nbElmnt-1);
-            }
-            else{ //sinon on ne fait que l'ajouter sur la map
-               map_set(pos_x,pos_y,k);
-            }
+    if (buffer) {
+      properties =  properties | MAP_OBJECT_GENERATOR;
+    }
 
-         }
-         else{//on a un 0 donc pas un élément
-            read(file,tmp,sizeof(char));
-            //on zappe et on passe à la suite maggle
-         }
+    //une fois toutes les variables récupérées via read, ajout de l'objet à la liste d'objet de la map
+    map_object_add(fichierPNG,nbSprite,properties);
+
+  }
+  map_object_end(); //fin création de la liste d'objet
+  free(fichierPNG); //libération du malloc utilisé pour le nom du fichier
+  //écriture sur la matrice de la map via map_set.
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      read(file,&buffer,sizeof(int));
+      if (buffer != MAP_OBJECT_NONE) {
+        map_set(i,j,buffer);
       }
-   }
-   //fin de lecture de tous les éléments.
-   map_object_end();
-   close(file);
-   //it_with_error ("Map load is not yet implemented\n");
+    }
+  }
+  close(file);
 }
+
 #endif
