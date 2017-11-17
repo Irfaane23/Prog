d53a353d2a992599d9d8ea797a8a5b6a37ae5478
Irfaane Ousseny <Irfaane@MacBook-Pro-de-Irfaane.local>
2017-11-17 16:53:33 +0100

    test

--- Projet2017/maps/maputil.c
+++ Projet2017/maps/maputil.c
@@ -17,24 +17,24 @@ int main(int argc, char** argv){
   read(file,&height,sizeof(int));
   read(file,&nbObject,sizeof(int));
 
-  if (argv[2]=="--getwidth") {
+  if (strcmp(argv[2],"--getwidth")==0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     printf("width  : %d\n", width);
   }
 
-  if (argv[2] =="--getheight") {
+  if (strcmp(argv[2] =="--getheight") == 0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     //read(file, &height,sizeof(int));
     printf("height  : %d\n", height);
   }
-  if (argv[2]== "--getobject") {
+  if (strcmp(argv[2]== "--getobject") == 0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     printf("nbObject :  %d\n", nbObject);
   }
-  if (argv[2]== "--getinfo") {
+  if (strcmp(argv[2]== "--getinfo") == 0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     printf("width  : %d\n", width);
@@ -42,7 +42,7 @@ int main(int argc, char** argv){
     printf("nbObjet  : %d\n", nbObject);
   }
 
-  if ((argv[2]=="--setwidth") && argc == 3) {
+  if (strcmp(argv[2]=="--setwidth") == 0 && argc == 3) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     width = atoi(argv[3]);
@@ -50,7 +50,7 @@ int main(int argc, char** argv){
     write(file,&width, sizeof(int));
     printf("width  : %d\n", width);
   }
-  if ((argv[2]=="--setheigth") && argc == 3 ) {
+  if (strcmp(argv[2]=="--setheigth") == 0 && argc == 3 ) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     height = atoi(argv[3]);
@@ -59,7 +59,7 @@ int main(int argc, char** argv){
     printf("height  : %d\n", height);
   }
 
-  if ((argv[2]== "--setwidth") && (argv[4]=="--setheight")) {
+  if (strcmp(argv[2]== "--setwidth") == 0 && strcmp(argv[4]=="--setheight") == 0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     width = atoi(argv[3]);
@@ -74,7 +74,7 @@ int main(int argc, char** argv){
 
   }
 
-  if ((argv[4]== "--setwidth") && (argv[2]== "--setheight")) {
+  if (strcmp(argv[4]== "--setwidth") == 0 && strcmp(argv[2]== "--setheight") == 0) {
     // on lit la valeur de map_width
     //read(file, &width,sizeof(int));
     width = atoi(argv[5]);
