f22c12dd8d31dd6a07d3e99baf640f851d5f69d4
Heritiana Mahazoasy <hmahazoasy@emi.u-bordeaux1.fr>
2017-12-04 14:34:37 +0100

    gérer le timer d'évenement

--- Projet2017/src/tempo.c
+++ Projet2017/src/tempo.c
@@ -25,7 +25,7 @@ static unsigned long get_time (void)
 #ifdef PADAWAN
 
 
-
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct event_list{
   void* evenement;
@@ -36,9 +36,9 @@ typedef struct event_list{
 LISTE *l = NULL;
 
 LISTE* new_event(long timer,void* event){
-  printf("\non est dans new_event");
+  //printf("\non est dans new_event");
   LISTE* lBis = (LISTE*) malloc(sizeof(LISTE));
-  printf("\nmalloc new_event ok");
+  //printf("\nmalloc new_event ok");
   lBis->evenement=event;
   lBis->tps=timer;
   lBis->next_event=NULL;
@@ -46,7 +46,8 @@ LISTE* new_event(long timer,void* event){
 }
 
 void untilNext(LISTE *l,Uint32 delay, void*param){
-  if(l->next_event!=NULL){
+  if(l->next_event != NULL){
+    printf("\nevenement : %p",l->evenement);
     untilNext(l->next_event,delay,param);
   }
   else{
@@ -54,48 +55,50 @@ void untilNext(LISTE *l,Uint32 delay, void*param){
   }
 }
 void *infiniteLoop(void *p){
-
+    //printf("\ndans infinite loop");
     sigset_t mask;
     //Blocage de tous les signaux
     sigemptyset(&mask);
     //on ajoute au mask le signal SIGALRM : qui esdl_push_eventst son seul signal
     pthread_sigmask(SIG_BLOCK, &mask, NULL);
-
+    //printf("\ndans infinite loop, avant le while");
   while(1){
+    //printf("\nc'est pas beau wallah");
     sigsuspend(&mask);
   }
 }
 
 void handler(int sig){
   printf("Thread courant : %p envoie le signal %d \n", pthread_self(), sig);
-  //printf ("sdl_push_event(%p) appelée au temps %ld\n", param, get_time ());
+
   sdl_push_event(&l);
 }
 // timer_init returns 1 if timers are fully implemented, 0 otherwise
 int timer_init (void)
 {
-  printf("\ndans timer_init");
+  //printf("\ndans timer_init");
   int nbThreads = 3;
   pthread_t t[nbThreads];
 
   //installation du handler pour le signal SIGALRM
-  printf("\ncréation du sigaction");
+  //printf("\ncréation du sigaction");
   struct sigaction s;
   s.sa_flags = 0;
   sigemptyset(&s.sa_mask);
   s.sa_handler = handler; // va afficher le thread courant
   sigaction(SIGALRM,&s,NULL);
-  printf("\nmask");
+  //printf("\nmask");
   sigset_t block;
   sigemptyset(&block);
   sigaddset(&block, SIGALRM);
   pthread_sigmask(SIG_BLOCK, &block, NULL);
 
   // Creaation du démon
-  printf("\njuste avant le loop");
+  //printf("\njuste avant le loop");
  for (int i = 0; i < nbThreads; i++) {
-   printf("\navant le premier")
+    //printf("\navant le premier create");
     pthread_create(t,NULL,infiniteLoop,NULL);
+    //printf("\naprès le premier create");
   }
 
   return 1; // Implementation not ready
@@ -103,36 +106,43 @@ int timer_init (void)
 
 timer_id_t timer_set (Uint32 delay, void *param)
 {
-  //structure du timer
-  struct itimerval timer;
 
-  //si le premier element de la liste est vide alors on
-  // creer un nouvel evenement
-  printf("\nadd event");
-  if(l->evenement == NULL){
+  //structure du timer
+
+  //printf("\njuste avant itimerval");
+  //pthread_mutex_lock(&mutex);
+  if(l == NULL){
     //creer un evenement
+    //printf("\nadd event");
      l=new_event(delay,param);
   }
   else{
     //on execute le signal
     untilNext(l,delay,param);
+    printf ("sdl_push_event(%p) appelée au temps %ld\n", param, get_time ());
   }
+  //pthread_mutex_unlock(&mutex);
 //  printf ("sdl_push_event(%p) appelée au temps %ld\n", param, get_time ());
   //sdl_push_event();
+  //printf("\nadd event");
 
- timer.it_value.tv_sec = 0;
- timer.it_value.tv_usec = 0;
 
- timer.it_interval.tv_sec = delay/1000;
- timer.it_interval.tv_usec = (delay%1000)/1000;
- setitimer(ITIMER_REAL, &timer, NULL);
+  ////// PAS TOUCHE /////////////
+
+  struct itimerval timer;
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 0;
+
+  timer.it_interval.tv_sec = delay/1000;
+  timer.it_interval.tv_usec = (delay%1000)/1000;
+  setitimer(ITIMER_REAL, &timer, NULL);
+
   return (timer_id_t) NULL;
 }
 
 int timer_cancel (timer_id_t timer_id)
 {
-  // TODO
-
+  //TODO
   return 0; // failure
 }
 
